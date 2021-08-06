7415a0d56443d1065172bec1d69e8ebccaad56bf
Klove Adams <kalebsxott@gmail.com>
2021-08-06 11:26:02 -0400

    setup a simple hook in useForm, now I remembed I am suposed to make it work

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 // write your custom hook here to control your checkout form
 
+export const useForm = initialValue => {
+    const [value, setValue] = useState(initialValue);
+    const handleChanges = updataValue => {
+        setValue(updataValue);
+    } 
+    return [value, setValue, handleChanges];
+}
